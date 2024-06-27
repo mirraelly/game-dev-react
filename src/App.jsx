9cc68f7f32e3c72417e7546671db56ebb094ac39
Mirraely <mirraelymoura@gmail.com>
2024-06-27 17:22:39 -0300

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Button from './components/Button/Button';
+import CourseInfo from './components/CourseInfo';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Nav from './components/Nav';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = function app () {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className='Container'>
+      <Button />
+      <CourseInfo />
+      <Footer />
+      <Header />
+      <MainContent />
+      <Nav />
+    </div>
+  );
+};
 
-export default App
+export default App;
