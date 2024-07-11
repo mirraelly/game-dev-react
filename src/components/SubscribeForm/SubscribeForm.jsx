2f1fa8d9acc4b022ca6ff93a578089e923ae0df1
Mirraely <mirraelymoura@gmail.com>
2024-07-11 08:39:50 -0300

    adds details to responsiveness and email form

--- src/components/SubscribeForm/SubscribeForm.jsx
+++ src/components/SubscribeForm/SubscribeForm.jsx
@@ -3,7 +3,7 @@ import './SubscribeForm.css';
 const SubscribeForm = () => {
     return (
         <form className='email-forms e-mail-edge'>
-            <input type="text" placeholder="Email Address" className='email-input bord e-mail-edge' required />
+            <input type="email" placeholder="Email Address" className='email-input bord e-mail-edge' required />
             <button type='submit' className='email-submit textWhite e-mail-edge'>Subscrever</button>
         </form>
     );
